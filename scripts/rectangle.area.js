cc3378cb7a814260264f931d5935a44d9ad12733
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-02-13 00:47:51 +0600

    reusable function added

--- scripts/rectangle.area.js
+++ scripts/rectangle.area.js
@@ -1,3 +1,16 @@
 function calculateRectangleArea() {
+    const RectangleWidthInput = document.getElementById('rectangle-width');
+    const RectangleWidthText = RectangleWidthInput.value;
+    const width = parseFloat(RectangleWidthText);
 
+    const RectangleLengthInput = document.getElementById('rectangle-length');
+    const RectangleLengthText = RectangleLengthInput.value;
+    const length = parseFloat(RectangleLengthText);
+
+    const area = length * width;
+    console.log(area);
+
+    // display triangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
