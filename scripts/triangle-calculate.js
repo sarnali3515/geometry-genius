1c69b8813eea6d78629bcec6095a5e71a0d9a43e
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-02-12 22:17:57 +0600

    triangle area calculted

---
+++ scripts/triangle-calculate.js
@@ -0,0 +1,17 @@
+function calculateTriangleArea() {
+    // console.log('inside triangle');
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    // display triangle area
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+}
